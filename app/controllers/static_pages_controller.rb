ff107fc76d9a7c4dd243ce861233fd549ec45497
Brantley Beaird <bbeaird@gmail.com>
2014-02-15 15:54:42 -0800

    Got example context.io code working for index page.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,14 @@
 class StaticPagesController < ApplicationController
   def index
+    contextio = ContextIO.new(ENV['CONTEXTIOKEY'], ENV['CONTEXTIOSECRET'])
+    account = contextio.accounts.where(email: ENV['MYEMAIL']).first
+    @subj_array = []
+
+    account.messages.where(limit: 5).each do |message|
+      @subj = message.subject
+      @subj_array << @subj
+    end
+
+
   end
 end
