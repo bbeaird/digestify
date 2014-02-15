6b05be722f3a24432bd4f497eae5d65000b3aae7
Brantley Beaird <bbeaird@gmail.com>
2014-02-15 15:02:43 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DevWeekHackathon::Application.config.secret_key_base = '20dbffd99e413842113a69ef464ee61b8c09086fad8b0fac238f5b1676ba43e4f8d6302b26271231f3043a02bf46f76aca40d99494d8a97539b37dbb981a4109'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DevWeekHackathon::Application.config.session_store :cookie_store, key: '_dev_week_hackathon_session'
